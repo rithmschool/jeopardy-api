a081016478f9ede934085983e9ee0131cf40209e
Elie Schoppik <eschoppik@gmail.com>
2022-07-22 14:57:39 -0700

    initial

---
+++ index.js
@@ -0,0 +1,38 @@
+const { categories, categoryDetail } = require("./constants");
+const PORT = process.env.PORT || 3000
+const express = require("express");
+const morgan = require("morgan")
+const app = express();
+const { NotFoundError } = require("./expressError");
+app.use(morgan("tiny"));
+
+app.get("/api/categories", function(req,res,next){
+  const numOfCategories = req.query.count || 5;
+  return res.send(categories.slice(0, numOfCategories))
+})
+
+app.get("/api/category", function (req, res, next) {
+  const categoryId = req.query.id
+  return res.send(categoryDetail[categoryId])
+})
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  throw new NotFoundError()
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+
+app.listen(PORT, function(){
+  console.log(`Server started on ${PORT}`)
+})
